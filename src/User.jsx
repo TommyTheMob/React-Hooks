142c72970f44fa839ed79f00c7e37a6ba73d5ff3
TommyTheMob <zv3rbrus@gmail.com>
2023-09-20 14:03:09 +0300

    add useEffect, useState

--- src/User.jsx
+++ src/User.jsx
@@ -1,26 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './user.scss'
 
-class User extends React.Component {
-    state = {
-        userData: null
-    }
+const User = ({ match }) => {
+    const [userData, setUserData] = useState(null)
 
-    componentDidMount() {
-        this.fetchUserData()
-    }
-
-    componentDidUpdate(prevProps) {
-        const prevUser = prevProps.match.params.userId
-        const currentUser = this.props.match.params.userId
-
-        if (prevUser !== currentUser ) {
-            this.fetchUserData()
-        }
-    }
-
-    fetchUserData = () => {
-        const {match} = this.props
+    useEffect(() => {
         fetch(`https://api.github.com/users/${match.params.userId}`)
             .then(response => {
                 if (response.ok) {
@@ -30,32 +14,29 @@ class User extends React.Component {
                 throw new Error()
             })
             .then(userData => {
-                this.setState({
-                    userData,
-                })
+                setUserData(userData)
             })
-    }
+    }, [match.params.userId])
+    
 
-    render() {
-        const { userData } = this.state
-        if (!userData) {
-            return null
-        }
-        const { name, location, avatar_url } = userData
-        return (
-            <div className="user">
-                <img
-                    src={avatar_url}
-                    alt="User Avatar"
-                    className="user__avatar"
-                />
-                <div className="user__info">
-                    <span className="user__name">{ name }</span>
-                    <span className="user__location">{ location }</span>
-                </div>
-            </div>
-        )
+    if (!userData) {
+        return null
     }
+    const { name, location, avatar_url } = userData
+    return (
+        <div className="user">
+            <img
+                src={avatar_url}
+                alt="User Avatar"
+                className="user__avatar"
+            />
+            <div className="user__info">
+                <span className="user__name">{ name }</span>
+                <span className="user__location">{ location }</span>
+            </div>
+        </div>
+    )
 }
+
 
 export default User
